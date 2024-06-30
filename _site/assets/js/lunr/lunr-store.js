f710bbe38c27098f9c914884df31e7c9eb887676
Juan Eduardo Rivera Perez <68937183+Eduardo99RP@users.noreply.github.com>
2024-06-30 16:40:38 -0600

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,1 @@
-var store = [{
-        "title": "Edge Case: Nested and Mixed Lists",
-        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["Edge Case"],
-        "tags": ["content","css","edge case","lists","markup"],
-        "url": "/edge%20case/edge-case-nested-and-mixed-lists/",
-        "teaser": null
-      },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/jekyll/update/welcome-to-jekyll/",
-        "teaser": null
-      }]
+var store = []
